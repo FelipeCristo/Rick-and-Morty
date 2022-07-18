e26b5d01d8ebeea8e05d81ccfb29a3129ff0b77e
FelipeCristo <felipe.cristo@yahoo.com.br>
2022-07-17 21:41:07 -0300

    create new user controller

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const connectDatabase = require("./database/database");
+const userRoute = require("./users/users.route");
 
 const port = process.env.PORT || 3001;
 const app = express();
 
+connectDatabase();
 app.use(cors());
 app.use(express.json());
+app.use("/users", userRoute);
+
+
 
 app.get("/", (req, res) => {
     res.send({ message: "Hello, world!" });
